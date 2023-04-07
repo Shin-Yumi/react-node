7e3bc1f76ed46485d949b44b08a00858ed4a0b3f
Shin-Yumi <tlsdbal0517@naver.com>
2023-04-07 22:37:01 +0900

    mongodb 오류 수정

--- server/model/postSchema.js
+++ server/model/postSchema.js
@@ -1,7 +1,3 @@
-//기존 일반 DB (Oracle, MySQL, MSSQL, MariaDB) 테이블 형식, SQL표준 명령어를 이용하여 DB입출력 관리
-//MongoDB : 자바스크립트의 객체형태로 데이터를 저장하는 구조, NoSQL, JS문으로 DB입출력 관리
-//Schema : 데이터베이스에 저장될 자료형식이나 키값을 강제하는 시스템적인 틀
-
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema(
@@ -9,8 +5,6 @@ const postSchema = new mongoose.Schema(
 		title: String,
 		content: String,
 		communityNum: Number,
-		//User컬랙션에서 참조하고자 하는 document의 object_id가 등록되면
-		//해당 다큐먼트의 정보를 post에서 참조
 		writer: {
 			ref: 'User',
 			type: mongoose.Schema.Types.ObjectId,
--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
 	{
-		displayName: String,
+		//displayName: String,
 		uid: String,
 		userNum: Number,
 	},
